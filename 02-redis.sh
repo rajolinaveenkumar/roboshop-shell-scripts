f5f2d889a9898d998e7a34d8dd2fd811c9635ca9
rajolinaveenkumar <rajolinaveenkumar6@gmail.com>
2025-06-05 15:13:40 +0530

    roboshop shell

--- 02-redis.sh
+++ 02-redis.sh
@@ -54,7 +54,7 @@ VALIDATE $? "enabling redis 7"
 dnf install redis -y 
 VALIDATE $? "Installing redis"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode on' /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 
 systemctl enable redis 
 VALIDATE $? "redis servive enable"
