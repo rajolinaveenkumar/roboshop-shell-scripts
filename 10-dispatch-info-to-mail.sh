6e7918128e4677af7fea3ffdebcf2594bc401df9
rajolinaveenkumar <rajolinaveenkumar6@gmail.com>
2025-06-06 10:50:32 +0530

    roboshop shell

--- 10-dispatch-info-to-mail.sh
+++ 10-dispatch-info-to-mail.sh
@@ -3,10 +3,10 @@
 log_line="/var/log/messages"
 
 # Extract JSON using grep + sed
-json=$(echo "$log_line" | grep -oP 'Order \K\{.*')
+json=$(echo $log_line | grep -oP 'Order \K\{.*')
 
 # Parse fields using jq
-order_id=$(echo "$json" | jq -r '.orderid')
+order_id=$(echo $json | jq -r '.orderid')
 user=$(echo "$json" | jq -r '.user')
 price=$(echo "$json" | jq -r '.cart.total')
 items=$(echo "$json" | jq -r '.cart.items[] | "\(.name) - ₹\(.price) x \(.qty)"')
